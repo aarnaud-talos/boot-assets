50d4cab885a3288e1bee90f390e97ab74bc068b3
Anthony ARNAUD <github@anthony-arnaud.fr>
2024-01-17 17:16:39 -0500

    remove unknown option

--- build.sh
+++ build.sh
@@ -22,7 +22,6 @@ make installer PLATFORM=linux/amd64 USERNAME=aarnaud-talos PKGS_PREFIX=ghcr.io/a
 make imager PLATFORM=linux/amd64 INSTALLER_ARCH=amd64 USERNAME=aarnaud-talos PKGS_PREFIX=ghcr.io/aarnaud-talos PKGS=${PKGS_VERSION} PKG_KERNEL=ghcr.io/aarnaud-talos/kernel:${PKGS_VERSION} PUSH=true
 
 docker run --rm -t -v $PWD/_out:/out ghcr.io/aarnaud-talos/imager:${TALOS_VERSION} metal --output-kind installer \
-  --output-image-options raw \
   --system-extension-image ghcr.io/siderolabs/intel-ucode:20231114 \
   --system-extension-image ghcr.io/siderolabs/i915-ucode:20231111 \
   --system-extension-image ghcr.io/aarnaud-talos/applesmc-t2:${EXTENSION_VERSION}
